50d49f715c9f915538d850ca73481cba56c73b7a
Frederic Stark <frederic.stark@gmail.com>
2023-08-07 17:06:44 +0200

    Added metadata and layouts

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+module.exports = {
+	title: "Fred's Retrocomputing",
+	url: "https://www.stark.fr/",
+	language: "en",
+	description: "My retro-computing blog",
+	author: {
+		name: "Fred Stark",
+		email: "frederic.stark@gmail.com",
+		url: "https://www.stark.fr/"
+	}
+}
